a228a993c2640b154933cca3b47f202fb6bb51a2
FullTimeScam <418.gingin@gmail.com>
2024-05-22 16:12:44 +0900

    the first Dapp - 지갑 연결하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { ethers } from "ethers";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const onClickMetamask = async () => {
+    try {
+      if (!window.ethereum) return;
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+
+      console.log(provider);
+
+      const signer = await provider.getSigner();
+
+      console.log(signer);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      <button onClick={onClickMetamask}>🦊 메타마스크 로그인</button>
+    </div>
+  );
 };
 
 export default App;
